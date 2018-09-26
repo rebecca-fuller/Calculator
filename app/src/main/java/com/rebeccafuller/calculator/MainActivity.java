5232cd56f4ccc0d8091780498cacdac8abcde56f
Becca Fuller <rebecca.fuller@mail.com>
2018-09-26 12:29:36 -0500

    Done!!!

--- app/src/main/java/com/rebeccafuller/calculator/MainActivity.java
+++ app/src/main/java/com/rebeccafuller/calculator/MainActivity.java
@@ -5,24 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
     TextView mathTV;
-    Button one, two, three, four, five, six, seven, eight, nine, zero, add, minus, multi, division, enter, clear;
 
-    String user, math, answer, numOneS, numTwoS;
+    Button zero, one, two, three, four, five, six, seven, eight, nine, decimal, addition, subtraction, multi, division, enter, clear;
 
-    int numOne, numTwo;
-    private int mNumOne;
-=======
->>>>>>> parent of 703e7c4... Finished UI
-=======
->>>>>>> parent of 703e7c4... Finished UI
-=======
->>>>>>> parent of 703e7c4... Finished UI
+    String sOne, sTwo, sAnswer, sOp, tempS;
+
+    Double dOne, dTwo, dAnswer;
+
+    Boolean mathPlace, useDouble;
+
+    int numOne, numTwo, numAnswer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
         mathTV = findViewById(R.id.math_TextView);
 
+        zero = findViewById(R.id.zero_Button);
         one = findViewById(R.id.one_Button);
         two = findViewById(R.id.two_Button);
         three = findViewById(R.id.three_Button);
@@ -40,159 +37,256 @@ public class MainActivity extends AppCompatActivity {
         seven = findViewById(R.id.seven_Button);
         eight = findViewById(R.id.eight_Button);
         nine = findViewById(R.id.nine_Button);
-        zero = findViewById(R.id.zero_Button);
 
-        add = findViewById(R.id.add_Button);
-        minus = findViewById(R.id.minus_Button);
-        division = findViewById(R.id.div_Button);
+        decimal = findViewById(R.id.decimal_Button);
+
+        addition = findViewById(R.id.plus_Button);
+        subtraction = findViewById(R.id.sub_Button);
         multi = findViewById(R.id.multi_Button);
+        division = findViewById(R.id.div_Button);
+
         enter = findViewById(R.id.enter_Button);
         clear = findViewById(R.id.clear_Button);
 
-        numOneS = null;
-        numTwoS = null;
+        sOne = null;
+        sTwo = null;
+        sAnswer = null;
+        sOp = null;
 
+        mathPlace = false;
+        useDouble = false;
+
+        zero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                insert(0);
+            }
+        });
         one.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (numOneS == null) {
-                    numOne = 1;
-                    setNumOne(numOne);
-                }
+                insert(1);
             }
         });
         two.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (numOneS == null) {
-                    numOne = 2;
-                    setNumOne(numOne);
-                }
+                insert(2);
             }
         });
         three.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (numOneS == null) {
-                    numOne = 3;
-                    setNumOne(numOne);
-                }
+                insert(3);
             }
         });
         four.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (numOneS == null) {
-                    numOne = 4;
-                    setNumOne(numOne);
-                }
+                insert(4);
             }
         });
         five.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (numOneS == null) {
-                    numOne = 5;
-                    setNumOne(numOne);
-                }
+                insert(5);
             }
         });
         six.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (numOneS == null) {
-                    numOne = 6;
-                    setNumOne(numOne);
-                }
+                insert(6);
             }
         });
         seven.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (numOneS == null) {
-                    numOne = 7;
-                    setNumOne(numOne);
-                }
+                insert(7);
             }
         });
         eight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (numOneS == null) {
-                    numOne = 8;
-                    setNumOne(numOne);
-                }
+                insert(8);
             }
         });
         nine.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (numOneS == null) {
-                    numOne = 9;
-                    setNumOne(numOne);
-                }
-            }
-        });
-        zero.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (numOneS == null) {
-                    numOne = 0;
-                    setNumOne(numOne);
-                }
+                insert(9);
             }
         });
 
-        add.setOnClickListener(new View.OnClickListener() {
+        decimal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                dec();
             }
         });
-        minus.setOnClickListener(new View.OnClickListener() {
+
+        addition.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                setOp("+");
             }
         });
-        division.setOnClickListener(new View.OnClickListener() {
+        subtraction.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                setOp("-");
             }
         });
         multi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                setOp("*");
+            }
+        });
+        division.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setOp("/");
             }
         });
 
         enter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                math();
             }
         });
         clear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                user = null;
-                math = null;
-                answer = null;
-                numOneS = null;
-                numTwoS = null;
-
-                mathTV.setText(null);
+                reset();
             }
         });
     }
 
-    public void setNumOne(int numOne) {
-        numOneS = String.valueOf(numOne);
+    private void insert(int i) {
+        if (!mathPlace) {
+            if (sOne == null) {
+                sOne = String.valueOf(i);
+            } else {
+                sOne+=i;
+            }
+        } else {
+            if (sTwo == null) {
+                sTwo = String.valueOf(i);
+            } else {
+                sTwo+=i;
+            }
+        }
+        setString();
 
-        user = numOneS + " ";
-
-        mathTV.setText(user);
     }
+
+    private void dec() {
+        useDouble = true;
+        if (!mathPlace) {
+            if (sOne == null) {
+                sOne = "0.";
+            } else {
+                sOne+=".";
+            }
+        } else {
+            if (sTwo == null) {
+                sTwo = "0.";
+            } else {
+                sTwo+=".";
+            }
+        }
+
+        setString();
+    }
+
+    private void setOp(String op) {
+        if (sOne != null) {
+            mathPlace = true;
+            sOp = op;
+
+            tempS = sOne + " " + sOp;
+            mathTV.setText(tempS);
+        } else {
+            Toast.makeText(this, "Please enter a number first.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void math() {
+        if (sTwo!= null) {
+            if (useDouble) {
+                dOne = Double.valueOf(sOne);
+                dTwo = Double.valueOf(sTwo);
+
+                switch (sOp) {
+                    case "+":
+                        dAnswer = dOne + dTwo;
+                        break;
+                    case "-":
+                        dAnswer = dOne - dTwo;
+                        break;
+                    case "*":
+                        dAnswer = dOne * dTwo;
+                        break;
+                    case "/":
+                        dAnswer = dOne / dTwo;
+                        break;
+                    default:
+                        break;
+                }
+
+                tempS = sOne + " " + sOp + " " + sTwo + " = " + String.valueOf(dAnswer);
+                mathTV.setText(tempS);
+            } else {
+                numOne = Integer.parseInt(sOne);
+                numTwo = Integer.parseInt(sTwo);
+
+                switch (sOp) {
+                    case "+":
+                        numAnswer = Math.addExact(numOne, numTwo);
+                        break;
+                    case "-":
+                        numAnswer = Math.subtractExact(numOne, numTwo);
+                        break;
+                    case "*":
+                        numAnswer = Math.multiplyExact(numOne, numTwo);
+                        break;
+                    case "/":
+                        numAnswer = Math.floorDiv(numOne, numTwo);
+                        break;
+                    default:
+                        break;
+                }
+
+                tempS = sOne + " " + sOp + " " + sTwo + " = " + String.valueOf(numAnswer);
+                mathTV.setText(tempS);
+            }
+        } else {
+            Toast.makeText(this, "Please enter your problem.", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void reset() {
+        sOne = null;
+        sTwo = null;
+        sAnswer = null;
+        sOp = null;
+
+        mathPlace = false;
+        useDouble = false;
+
+        mathTV.setText("");
+    }
+
+    private void setString() {
+        if (!mathPlace) {
+            mathTV.setText(sOne);
+        } else {
+            tempS = sOne + " " + sOp + " " + sTwo;
+            mathTV.setText(tempS);
+        }
+    }
+
+
 }
